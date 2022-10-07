e51fdc94e32b1d4331f3bbd1234c85f168331664
Gatien <78294761+GatienDuh@users.noreply.github.com>
2022-10-07 11:50:11 +0200

    Cuves début de code

--- Assets/_Scripts/CuveTest.cs
+++ Assets/_Scripts/CuveTest.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CuveTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Liste de l'ordre des boutons")]
+    [SerializeField] int[] pushButtonListSolution = new int[3];
 
-    // Update is called once per frame
-    void Update()
+    int[] pushButtonList = new int[] {-1, -1, -1};
+
+    int number = 0;
+    int isGood = 0;
+
+    public void pushButton(int buttonNumber)
     {
-        
+        if (buttonNumber == pushButtonListSolution[number])
+        {
+            pushButtonList[number] = buttonNumber;
+            number++;
+        }
+        else
+        {
+            number = 0;
+            pushButtonList = new int[] { -1, -1, -1};
+        }
+
+        for (int i = 0; i < pushButtonListSolution.Length; i++)
+        {
+            if (pushButtonList[i] == pushButtonListSolution[i])
+            {
+                isGood++;
+            }
+        }
+
+        if (isGood == 3)
+        {
+            //Cuves OK
+        }
+        else if (isGood < 3)
+        {
+            isGood = 0;
+        }
     }
 }
