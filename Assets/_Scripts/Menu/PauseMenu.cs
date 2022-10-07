48278eb2b74ca0b4270be01261112800de149b84
Tenjizen <92018813+Tenjizen@users.noreply.github.com>
2022-10-07 11:56:14 +0200

    Pause menu
    Okey
    reste le "menu" avec les ingredients et les notes a faire

--- Assets/_Scripts/Menu/PauseMenu.cs
+++ Assets/_Scripts/Menu/PauseMenu.cs
@@ -7,16 +7,24 @@ public class PauseMenu : MonoBehaviour
     public static bool gameIsPaused = false;
 
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject credits;
+    [SerializeField] GameObject settings;
+    //[SerializeField] GameObject pauseMenu;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            gameIsPaused = !gameIsPaused;
-            PauseGame();
+            if (settings.gameObject.activeInHierarchy || credits.gameObject.activeInHierarchy)
+                MenuPause();
+            else
+            {
+                gameIsPaused = !gameIsPaused;
+                PauseGame();
+            }
         }
     }
-    void PauseGame()
+    public void PauseGame()
     {
         if (gameIsPaused)
         {
@@ -26,8 +34,38 @@ public class PauseMenu : MonoBehaviour
         else
         {
             Time.timeScale = 1;
-            pauseMenu.SetActive(false
-                );
+            pauseMenu.SetActive(false);
         }
+    }
+    public void Resume()
+    {
+        gameIsPaused = !gameIsPaused;
+        PauseGame();
+    }
+    public void MenuPause()
+    {
+        pauseMenu.SetActive(true);
+        credits.SetActive(false);
+        settings.SetActive(false);
+    }
+    public void Options()
+    {
+        pauseMenu.SetActive(false);
+        settings.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        pauseMenu.SetActive(false);
+        settings.SetActive(false);
+        credits.SetActive(true);
+    }
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }
