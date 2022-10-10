ab2dd0f060bd601fa847a40e335771dffbfe99a2
Gatien <78294761+GatienDuh@users.noreply.github.com>
2022-10-10 11:03:56 +0200

    SceneManagement

--- Assets/_Scripts/NextScene.cs
+++ Assets/_Scripts/NextScene.cs
@@ -10,7 +10,7 @@ public class NextScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene(nextScene);
         }
